bc51aaf73d190b43ef9d870acb07a9b489e213be
Norphel <113838216+norphel@users.noreply.github.com>
2023-09-26 12:32:39 +0530

    test for ship's properties and methods

--- tests/Ship.js
+++ tests/Ship.js
@@ -2,5 +2,13 @@ class Ship {
     constructor(length) {
         this.length = length;
     }
+    noOfTimesHit = 0
+    sunk = false
+    hit() {
+        //hit 
+    }
+    isSunk() {
+        //isSunk
+    }
 }
 module.exports = Ship;
--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -1,8 +1,29 @@
 const Ship = require('./Ship');
 
-test('check ship has length property', () => {
-    expect(new Ship(5)).toHaveProperty('length');
+let ship = undefined;
+beforeEach( () => {
+    ship = new Ship(5)
 })
-test('check length property is correctly initialized', () => {
-    expect((new Ship(4)).length).toBe(4);
+
+describe('check ship has required properties and methods', () => {
+
+    test('check ship has length property with correct value', () => {
+        expect(ship).toHaveProperty('length', 5);
+    })
+
+    test('check ship has noOfTimesHit property with value 0 initially', () => {
+        expect(ship).toHaveProperty('noOfTimesHit', 0)
+    })
+
+    test('check ship has sunk property with value false initially', () => {
+        expect(ship).toHaveProperty('sunk', false)
+    })
+
+    test('check ship has hit method', () => {
+        expect(ship).toHaveProperty('hit')
+    })
+
+    test('check ship has isSunk method', () => {
+        expect(ship).toHaveProperty('isSunk')
+    })
 })
