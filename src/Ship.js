36e59d52a4b65f703907b7a55a4191f99c07319f
Norphel <113838216+norphel@users.noreply.github.com>
2023-09-27 10:49:21 +0530

    refactor Ship to make properties private

---
+++ src/Ship.js
@@ -0,0 +1,21 @@
+class Ship {
+    constructor(length) {
+        let _length = length;
+        let _numberOfTimesHit = 0;
+        let _sunk = false;
+    
+        this.hit = function() {
+            _numberOfTimesHit += 1;
+            if (_numberOfTimesHit >= _length) {
+                _sunk = true
+            }
+        };
+        this.isSunk = function() {
+            return _numberOfTimesHit >= _length;
+        };
+        this.getNumberOfHits = function() {
+            return _numberOfTimesHit;
+        };
+    }
+}
+module.exports = Ship;
--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -1,4 +1,4 @@
-const Ship = require('./Ship');
+const Ship = require('../src/Ship');
 
 let ship = undefined;
 beforeEach( () => {
@@ -6,19 +6,7 @@ beforeEach( () => {
 })
 
 //probably redundant but let's keep for now
-describe('check ship has required properties and methods', () => {
-
-    test('check ship has length property with correct value', () => {
-        expect(ship).toHaveProperty('length', 5);
-    })
-
-    test('check ship has numberOfTimesHit property with value 0 initially', () => {
-        expect(ship).toHaveProperty('numberOfTimesHit', 0)
-    })
-
-    test('check ship has sunk property with value false initially', () => {
-        expect(ship).toHaveProperty('sunk', false)
-    })
+describe('check ship has required public methods', () => {
 
     test('check ship has hit method', () => {
         expect(ship).toHaveProperty('hit')
@@ -32,13 +20,13 @@ describe('check ship has required properties and methods', () => {
 describe('test hit method', () => {
     test('hit method increments numberOfTimesHit property', () => {
         ship.hit();
-        expect(ship.numberOfTimesHit).toBe(1)
+        expect(ship.getNumberOfHits()).toBe(1)
     })
     test('hit method increments numberOfTimesHit property multiple times when hit called multiple times', () => {
         ship.hit();
         ship.hit();
         ship.hit();
-        expect(ship.numberOfTimesHit).toBe(3)
+        expect(ship.getNumberOfHits()).toBe(3)
     })
     test('when ship is hit equal to or greater than its length it should sink', () => {
         ship.hit();
@@ -46,7 +34,7 @@ describe('test hit method', () => {
         ship.hit();
         ship.hit();
         ship.hit();
-        expect(ship.sunk).toBe(true)
+        expect(ship.isSunk()).toBe(true)
     })
 })
 
