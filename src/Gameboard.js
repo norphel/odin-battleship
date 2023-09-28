c4c072784a979ad7246cc18b3f39de42f1206a4e
Norphel <113838216+norphel@users.noreply.github.com>
2023-09-28 11:06:03 +0530

    update placeShip to consider overlapping

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -9,6 +9,10 @@ class Gameboard {
         this.placeShip = function(ship, row, col, isHorizontal) {
             const shipLength = ship.getLength();
 
+            if (this.isOverlapping(ship, row, col, isHorizontal)) {
+                throw new Error('Overlapping');
+            }
+
             if (isHorizontal) {
                 for (let i = 0; i < shipLength; i++) {
                     _board[row][col + i] = ship;
@@ -18,12 +22,32 @@ class Gameboard {
                     _board[row + i][col] = ship;
                 }
             }
-            
         };
 
         this.getShipAt = function(row, col) {
             return _board[row][col];
         };
+
+        this.isOverlapping = function(ship, row, col, isHorizontal) {
+            const shipLength = ship.getLength();
+            
+            if (isHorizontal) {
+                for (let i = 0; i < shipLength; i++) {
+                    if (_board[row][col + i] !== null) {
+                        return true; 
+                    }
+                }
+            } else {
+                for (let i = 0; i < shipLength; i++) {
+                    if (_board[row + i][col] !== null) {
+                        return true;
+                    }
+                }
+            }
+            
+            return false; 
+        }
+
     }
 }
 module.exports = Gameboard;
